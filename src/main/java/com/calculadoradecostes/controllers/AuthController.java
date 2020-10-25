7b03ad7f3df7c2326de7d872b6239b21135cec64
Juan Medina <soyjuanmedina@gmail.com>
2020-10-25 18:53:17 +0100

    Remove Comment

--- src/main/java/com/calculadoradecostes/controllers/AuthController.java
+++ src/main/java/com/calculadoradecostes/controllers/AuthController.java
@@ -32,7 +32,6 @@ import com.calculadoradecostes.repository.UserRepository;
 import com.calculadoradecostes.security.jwt.JwtUtils;
 import com.calculadoradecostes.security.services.UserDetailsImpl;
 
-//Comentario
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
