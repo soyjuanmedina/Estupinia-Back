08b9ddd7291494236a1d69f76427a0ce97ee9456
juan.medina <juan.medina@commonms.com>
2021-04-28 12:59:04 +0200

    Rest premium articles read

--- src/main/java/com/columns/controllers/ArticleController.java
+++ src/main/java/com/columns/controllers/ArticleController.java
@@ -1,37 +1,47 @@
 package com.columns.controllers;
 
 import java.util.List;
-import java.util.Optional;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.columns.models.Article;
-import com.columns.models.Params;
 import com.columns.models.User;
+import com.columns.repository.UserRepository;
 import com.columns.security.services.ArticleService;
-import com.columns.services.EmailService;
 
 @RestController
 @RequestMapping("/article")
 @CrossOrigin(origins = "*")
 public class ArticleController {
-	
 
 	@Autowired
 	ArticleService articleService;
-	
+
+	@Autowired
+	UserRepository userRepository;
+
 	@PostMapping("/recomended")
-	public  ResponseEntity<List<Article>> getRecomendedArticles() {
+	public ResponseEntity<List<Article>> getRecomendedArticles() {
 		List<Article> recomendedArticles = articleService.getRecomendedArticles();
 		return ResponseEntity.ok(recomendedArticles);
+	}
+
+	@PostMapping("/confirmreadpremium")
+	public ResponseEntity<Boolean> confirmReadPremium() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String currentPrincipalName = authentication.getName();
+		User user = userRepository.findByUsername(currentPrincipalName).get();
+		user.setPremium_remain(user.getPremium_remain() - 1);
+		userRepository.save(user);
+		Boolean response = true;
+		return ResponseEntity.ok(response);
 	}
 
 }
